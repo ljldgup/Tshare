2c5fe70050462bfda9879733cd902ab88629e407
ljldgup <1142509832@qq.com>
2018-10-22 22:46:57 +0800

    add json time filter;remove some bugs

--- HelloWorld/tshare/views.py
+++ HelloWorld/tshare/views.py
@@ -12,45 +12,15 @@ def trade_report(request):
 	if dbrefreshed == False:
 		dbtools.refresh_k_data('k_bfq',)
 		dbrefreshed = True
-		
+
+	st_date = request.GET['st_date']
+	ed_date = request.GET['ed_date']
+	
 	data = {}
-	
-	# 当提交表单时
-	st_date = None
-	ed_date = None
-	r_name =None
-	stat_data = tmd.StatisticTradeData.objects.all().order_by('o_date')
-	
-	#买入时间大于等于st_date
-	try:
-		st_date = request.GET['st_date']
-		if st_date != "":
-			st_date_dtm = datetime.strptime(st_date, "%Y-%m-%d")
-			stat_data = stat_data.filter(i_date__gte = st_date_dtm)
-	except BaseException as e:
-		print(e)
-	
-	#卖出时间小于等于ed_date	
-	try:
-		ed_date = request.GET['ed_date']
-		if ed_date != "":
-			ed_date_dtm = datetime.strptime(ed_date, "%Y-%m-%d") 
-			stat_data = stat_data.filter(o_date__lte = ed_date_dtm)
-	except BaseException as e:
-		print(e)
-		
-	try:
-		r_name = request.GET['r_name']
-		if r_name != "":
-			stat_data = stat_data.filter(name = r_name)
-	except BaseException as e:
-		print(e)
 
 	#保存页面数据
 	data['st_date'] = st_date
 	data['ed_date'] = ed_date
-	data['r_name'] = r_name
-	data['stat_data'] = stat_data;
 	return render(request, 'tshare/trade_report.html',data)
 	
 def trade_detail(request,r_code):
@@ -111,6 +81,25 @@ def trade_data_json(request):
 
 	#store_k_data to tmp table
 	stat_data = tmd.StatisticTradeData.objects.all().order_by('o_date')
+	
+	#买入时间大于等于st_date
+	try:
+		st_date = request.GET['st_date']
+		if st_date != "":
+			st_date_dtm = datetime.strptime(st_date, "%Y-%m-%d")
+			stat_data = stat_data.filter(i_date__gte = st_date_dtm)
+	except BaseException as e:
+		print(e)
+	
+	#卖出时间小于等于ed_date	
+	try:
+		ed_date = request.GET['ed_date']
+		if ed_date != "":
+			ed_date_dtm = datetime.strptime(ed_date, "%Y-%m-%d") 
+			stat_data = stat_data.filter(o_date__lte = ed_date_dtm)
+	except BaseException as e:
+		print(e)
+		
 	stat_list = []
 	for i in stat_data:
 		stat_list.append([i.code,i.name,str(i.i_date),str(i.o_date),i.i_total,i.time,i.earning,i.pct])
